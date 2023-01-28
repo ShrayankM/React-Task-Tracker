dbf253f9106018df517b7bc1404649ad9f8efa99
ShrayankM <shraymist@gmail.com>
2023-01-28 23:00:48 +0530

    added json db server for mocking backend

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,46 +1,73 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState(
-    [{
-      id: 1,
-      text: 'Doctors Appointment' ,
-      day: 'Feb 5th at 2.30pm',
-      remainder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 1.30pm',
-      remainder: true,
-    },
-    {
-      id: 3,
-      text: 'Food Shopping',
-      day: 'Feb 5th at 2.30pm',
-      remainder: false,
-    }]);
-  
-  //TODO Add task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10) + 1;
-    const newTask = {id, ...task}
+  const [tasks, setTasks] = useState([]);
 
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    }
+    getTasks()
+  }, []);
+  
+
+  //TODO Fetch tasks data from server
+  const fetchTasks = async () => {
+    const response = await fetch('http://localhost:5000/tasks');
+    const data = await response.json();
+
+    return data;
+  }
+
+  //TODO Fetch single task from server
+  const fetchTask = async (id) => {
+    const response = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await response.json();
+
+    return data;
+  }
+
+  //TODO Add task
+  const addTask = async (task) => {
+    const response = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(task)
+    });
+    
+    const newTask = await response.json()
     setTasks([...tasks, newTask]);
+
+    // const id = Math.floor(Math.random() * 10) + 1;
+    // const newTask = {id, ...task}
   }  
   
   //TODO Delete task functionality  
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, { method: 'DELETE' });
+
     setTasks(tasks.filter((task) => task.id !== id));
   }
 
   //TODO Toggle Remainder on Double Click on check
-  const toggleRemainder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? {...task, remainder: !task.remainder} : task));
+  const toggleRemainder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = {...taskToToggle, remainder: !taskToToggle.remainder}
+
+    const response = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(updatedTask)
+    });
+
+    const data = await response.json();
+
+    setTasks(tasks.map((task) => task.id === id ? {...task, remainder: !data.remainder} : task));
   }
 
   return (
